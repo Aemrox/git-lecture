bbd3391d52f4177d89dbf5b113a5391e4ff55877
Aemrox <adam.may2164@gmail.com>
2015-11-30 16:37:43 -0500

    more notes

--- hello.rb
+++ hello.rb
@@ -6,3 +6,9 @@
 
 # getting out of the weird less format that git uses (I.E. (END)) just type
 # q - only q - and it will get you out
+
+
+# git diff allows you to see the difference between different versions in your repo
+#
+# git reset allows you to unstage changes before you commit
+# git reset HEAD to unstage deleted stuff
